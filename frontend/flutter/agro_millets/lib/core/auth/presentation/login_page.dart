f68d6923616c92ffc4d14c5c00bdbd59da254da3
amannegi <akuro787898@gmail.com>
2023-03-27 02:55:50 +0530

    feat: Add Home and Add Item Page

--- frontend/flutter/agro_millets/lib/core/auth/presentation/login_page.dart
+++ frontend/flutter/agro_millets/lib/core/auth/presentation/login_page.dart
@@ -4,21 +4,22 @@ import 'package:agro_millets/globals.dart';
 import 'package:agro_millets/widgets/action_button.dart';
 import 'package:agro_millets/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  ConsumerState<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends ConsumerState<LoginPage> {
   late AuthManager _authManager;
   String email = "", password = "";
 
   @override
   void initState() {
-    _authManager = AuthManager(context);
+    _authManager = AuthManager(context, ref);
     super.initState();
   }
 
@@ -48,8 +49,19 @@ class _LoginPageState extends State<LoginPage> {
               text: "Login",
             ),
             SizedBox(height: 0.015 * getHeight(context)),
+            ActionButton(
+              onPressed: () async {
+                await _authManager.googleAuth();
+              },
+              text: "Login using Google",
+            ),
+            SizedBox(height: 0.015 * getHeight(context)),
             GestureDetector(
-              onTap: () => goToPage(context, const SignUpPage()),
+              onTap: () => goToPage(
+                context,
+                const SignUpPage(),
+                clearStack: true,
+              ),
               child: RichText(
                 text: TextSpan(
                   style: Theme.of(context).textTheme.bodyMedium,
--- frontend/flutter/agro_millets/lib/main.dart
+++ frontend/flutter/agro_millets/lib/main.dart
@@ -1,39 +1,47 @@
 import 'package:agro_millets/core/auth/presentation/login_page.dart';
+import 'package:agro_millets/core/home/presentation/home_page.dart';
+import 'package:agro_millets/data/auth_state_repository.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await EasyLocalization.ensureInitialized();
 
   runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('hi')],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('en'),
-      child: const App(),
+    ProviderScope(
+      child: EasyLocalization(
+        supportedLocales: const [Locale('en'), Locale('hi')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'),
+        child: const App(),
+      ),
     ),
   );
 }
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: "Agro-Millets",
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
       debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(useMaterial3: true,
-      fontFamily: GoogleFonts.poppins().fontFamily,
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: GoogleFonts.poppins().fontFamily,
       ),
-      home: const LoginPage(),
+      home: ref.watch(authProvider).isLoggedIn()
+          ? const HomePage()
+          : const LoginPage(),
     );
   }
 }
