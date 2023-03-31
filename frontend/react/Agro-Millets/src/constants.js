d8edae930be74fe9af8973dca369d35232661374
amannegi <akuro787898@gmail.com>
2023-03-31 13:41:30 +0530

    fix: Update API URL's

--- frontend/react/Agro-Millets/src/constants.js
+++ frontend/react/Agro-Millets/src/constants.js
@@ -1,1 +1,5 @@
-export const API_URL = "http://localhost:3000/api";
+//TODO: Use below Url for local development
+// export const API_URL = "http://localhost:3000/api";
+
+// Original API URL
+export const API_URL = "https://agro-millets.adaptable.app/api";
