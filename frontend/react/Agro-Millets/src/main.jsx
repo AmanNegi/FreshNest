1a609fd49c3cef00f08be09cecfb4275d5957b74
amannegi <akuro787898@gmail.com>
2023-03-31 11:31:33 +0530

    feat: Add Shop, Home and About Pages

--- frontend/react/Agro-Millets/src/main.jsx
+++ frontend/react/Agro-Millets/src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Login from "./pages/Login/presentation/Login";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/Home/presentation/Home";
+import Shop from "./pages/shop/presentation/Shop";
+import About from "./pages/About/presentation/about";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-  <h1 className='text-center text-orange-400'>Agro Millets</h1>
-  </>
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <main className="font-poppins box-border smooth-scroll h-[100%] w-[100%]">
+    <RouterProvider router={router} />
+  </main>
+);
---
+++ frontend/react/Agro-Millets/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blackColor: "#020202",
+        darkColor: "#0d2818",
+        semiDarkColor: "#0d2818",
+        lightColor: "#058c42",
+        accentColor: "#16db65",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
