e7aab5073bd2e837bc4fb6ab3edf6ba25ae09a91
amannegi <akuro787898@gmail.com>
2023-03-31 11:30:37 +0530

    fix: Update API's response

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
