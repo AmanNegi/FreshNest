462c0c2026db89e14309682049667820645236b0
amannegi <akuro787898@gmail.com>
2023-03-31 13:57:16 +0530

    fix: Update Netlify Settings

--- frontend/react/Agro-Millets/src/pages/Login/application/auth.js
+++ frontend/react/Agro-Millets/src/pages/Login/application/auth.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../../constants";
 
 // http://localhost:3000/api/list/getAll
 
 export default async function login(email, password) {
-  var res = await axios.post("http://localhost:3000/api/auth/login", {
+  var res = await axios.post(API_URL + "/auth/login", {
     email: email,
     password: password,
   });
