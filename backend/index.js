f02e3e5752fe89a39a5290a0c27eb7c3bb877fcc
amannegi <akuro787898@gmail.com>
2023-06-22 17:40:35 +0530

    fix: Add cors specificity

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,14 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://agro-millets.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    headers: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(bodyParser.json());
 
