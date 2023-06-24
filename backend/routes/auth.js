c8ab048d024749cd3b32dfd5217cf9bbf2d1b146
amannegi <akuro787898@gmail.com>
2023-06-24 17:05:07 +0530

    fix: GLogin Issue

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -32,7 +32,7 @@ router.post("/login", async (req, res) => {
 });
 
 /**
- * SignUp a new user
+ * Sign up a new user
  * BODY: { name, email, password, userType, phone}
  */
 
@@ -57,6 +57,47 @@ router.post("/signup", async (req, res) => {
     password: req.body.password,
     userType: userType,
     phone: req.body.phone,
+  });
+
+  await user.save();
+  return res.send(
+    getSuccessResponse(
+      "Signup Successful",
+      _.omit(user.toObject(), ["password", "__v"])
+    )
+  );
+});
+
+/**
+ * Save credentials for a user (Google Login)
+ * { email, name}
+ */
+
+router.post("/saveGLogin", async (req, res) => {
+  console.log("Request Body: ", req.body);
+  const { error } = validateGLogin(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.send(
+      getSuccessResponse(
+        "Login Success",
+        _.omit(user.toObject(), ["password", "__v"])
+      )
+    );
+  }
+
+  const userType = "customer";
+  const email = req.body.email;
+
+  // By default email is the password itself
+  user = new User({
+    email: email,
+    name: req.body.name,
+    password: email,
+    userType: userType,
+    phone: "xxxxxxxxxx",
   });
 
   await user.save();
@@ -95,13 +136,7 @@ router.post("/getAll", async (req, res) => {
 
   var users = await User.find({}).select("-__v -password");
 
-  return res.send(
-    getSuccessResponse(
-      "Success",
-      users
-      // _.omit(user.toObject(), ["password", "__v"])
-    )
-  );
+  return res.send(getSuccessResponse("Success", users));
 });
 
 router.post("/exists", async (req, res) => {
@@ -135,6 +170,15 @@ function validateSignUp(req) {
     phone: Joi.string().required(),
     userType: Joi.string().default("customer"),
   });
+  return schema.validate(req);
+}
+
+function validateGLogin(req) {
+  const schema = Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+  });
+
   return schema.validate(req);
 }
 
