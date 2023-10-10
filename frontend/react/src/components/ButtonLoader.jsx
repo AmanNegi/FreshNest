8203803a91c1e93f3d4da1c96b6ecf57e2ee448e
Adegoke Ayobami <adegokeayobamiseun>
2023-10-10 11:39:39 +0100

    changed loader to DaisyUI  Spinner

--- frontend/react/src/components/ButtonLoader.jsx
+++ frontend/react/src/components/ButtonLoader.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Circles } from "react-loader-spinner";
 
 function ButtonLoader() {
   return (
     <>
-      <Circles
-        height="30"
-        width="30"
-        color="#4fa94d"
-        ariaLabel="circles-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
+      <span className="loading loading-spinner text-warning"></span>
     </>
   );
 }
