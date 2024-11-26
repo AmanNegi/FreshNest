76b33dac164fc6823219c60814aa3cd9d0435ed5
amannegi <akuro787898@gmail.com>
2024-11-26 18:33:22 +0530

    fix: minor backend bug

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const logger = require('./src/utils/logger')
-const dotenv = require('dotenv')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("./src/utils/logger");
+const dotenv = require("dotenv");
 
-const env = process.env.NODE_ENV || 'prod'
-dotenv.config({ path: `.env.${env}` })
+const env = process.env.NODE_ENV || "prod";
+dotenv.config({ path: `.env.${env}` });
 
-const app = express()
+const app = express();
 
-if (env === 'dev') {
-  const swaggerUi = require('swagger-ui-express')
-  const swaggerFile = require('./src/swagger_output.json')
+if (env === "dev") {
+  const swaggerUi = require("swagger-ui-express");
+  const swaggerFile = require("./src/swagger_output.json");
 
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 }
 
 const corsPrefs = cors({
-  origin: ['https://fresh-nest.netlify.app', 'http://localhost:5173'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  headers: ['Content-Type', 'Authorization']
-})
+  origin: ["https://fresh-nest.netlify.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  headers: ["Content-Type", "Authorization"],
+});
 
 const customLogger = (req, _, next) => {
-  logger.info(`${req.method} ${req.url}`)
-  next()
-}
+  logger.info(`${req.method} ${req.url}`);
+  next();
+};
 
-app.use(corsPrefs)
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(customLogger)
-require('./src/startup/routes')(app)
+app.use(corsPrefs);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(customLogger);
+require("./src/startup/routes")(app);
 
-if (env !== 'test') require('./src/startup/db')()
+if (env !== "test") require("./src/startup/db")();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-module.exports = app.listen(port, () => console.log(`Listening on Port ${port}...`))
+const expressApp = app.listen(port, () =>
+  console.log(`Listening on Port ${port}...`)
+);
+
+module.exports = expressApp;
